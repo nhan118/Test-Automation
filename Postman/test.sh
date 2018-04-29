e29f0d3c39777fb0af674e6933c8ad6ed2da069e
HanNing <themail@sohu.com>
2018-04-29 21:59:31 +0800

    add junit report

--- Postman/test.sh
+++ Postman/test.sh
@@ -1,5 +1,5 @@
-newman run demo.postman_collection.json -e test.postman_environment.json -r cli,html --reporter-html-export ./report/report.html
+newman run demo.postman_collection.json -e test.postman_environment.json -r cli,junit --reporter-junit-export ./report/report.xml
 sleep 3
-newman run demo.postman_collection1.json -e test.postman_environment.json -r cli,html --reporter-html-export ./report/report1.html
+newman run demo.postman_collection1.json -e test.postman_environment.json -r cli,junit --reporter-junit-export ./report/report1.xml
 sleep 3
-newman run demo.postman_collection2.json -e test.postman_environment.json -r cli,html --reporter-html-export ./report/report2.html
+newman run demo.postman_collection2.json -e test.postman_environment.json -r cli,junit --reporter-junit-export ./report/report2.xml
