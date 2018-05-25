f55ba3e64b3c19aa773ceaa48f4cd91edb850e82
HanNing <themail@sohu.com>
2018-05-25 15:43:01 +0800

    delete autoline

--- Postman/run.sh
+++ Postman/run.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
- echo "run postman scripts"
+
+echo "run postman scripts"
+IFS='
+'
+for line in `cat caselist.csv`
+do
+	#echo $line
+	#echo $line | xargs -t newman run
+	echo "newman run "${line} | cat >> test1.sh
+	
+	#sleep 3
+done
+
+./test1.sh
+
