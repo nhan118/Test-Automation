be5e96dccba68fcd3a90dd48f9bc1fe997e98afc
HanNing <themail@sohu.com>
2018-05-29 17:30:23 +0800

    update .gitignore

---
+++ Selenium/automation_test_framework/testsuites/TestRunner.py
@@ -0,0 +1,10 @@
+# coding = utf-8
+import unittest
+
+suite = unittest.TestLoader().discover("testsuites")
+
+
+if __name__=='__main__':
+    #执行用例
+    runner=unittest.TextTestRunner()
+    runner.run(suite)
